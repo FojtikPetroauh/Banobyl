71c56e9b34c3db61de7cace6951d344e5f966ccb
FojtikPetroauh <fojtik_petr@oauh.cz>
2026-03-05 16:47:35 +0100

    Vzhledové předělání mapy, přidání funkčnosti na doplnění hladu a žízně

--- Assets/_Scripts/SimpleInventory.cs
+++ Assets/_Scripts/SimpleInventory.cs
@@ -7,7 +7,32 @@ public class SimpleInventory : MonoBehaviour
     public int waterCount = 0;
     public int woodCount = 0;
 
-    
+    public SurvivalManager survivalManager;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if(berriesCount > 0)
+            {
+                berriesCount--;
+                survivalManager.replenishHunger(20f);
+                Debug.Log("You ate a berry. Remaining: " + berriesCount);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if(waterCount > 0)
+            {
+                waterCount--;
+                survivalManager.replenishThirst(50f);
+                Debug.Log("You drank water. Remaining: " + waterCount);
+            }
+        }
+    }
+
+
     public void AddResource(string type, int amount)
     {
         switch (type)
