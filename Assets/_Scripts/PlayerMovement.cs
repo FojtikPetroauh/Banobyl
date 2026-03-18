6ebbc97f35abb5beebb1284b32c09d9147fe0c3c
FojtikPetroauh <fojtik_petr@oauh.cz>
2026-03-18 19:42:25 +0100

    Přidání animace chůze postavy + předělání logiky scriptu na pohyb

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -2,19 +2,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Nastavení pohybu")]
     public float moveSpeed = 5f;
     public Rigidbody2D rb;       
 
-    Vector2 movement;
+    private Animator anim;
+    private Vector2 movement;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (anim != null)
+        {
+            if (movement.x != 0 || movement.y != 0)
+            {
+                anim.SetFloat("MoveX", movement.x);
+                anim.SetFloat("MoveY", movement.y);
+                
+                anim.speed = 1f; 
+            }
+            else
+            {
+                anim.speed = 0f; 
+            }
+        }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
     }
 }
